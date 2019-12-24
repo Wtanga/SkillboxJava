90981fce6a613d3e7bb96e35c7c157c3a37889d9
Wtanga <47683859+Wtanga@users.noreply.github.com>
2019-12-24 15:30:26 +0700

    Add files via upload

--- Module 6/bank/src/Main.java
+++ Module 6/bank/src/Main.java
@@ -1,20 +1,24 @@
+import Accounts.CardAccount;
+import Accounts.DefaultAccount;
+import Accounts.DepositAccount;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Default acc");
         DefaultAccount acc = new DefaultAccount();
         acc.deposit(10000);
+        acc.withdraw(100000.0);
         acc.checkBalance();
-        acc.deposit(-100);
         System.out.println("Card acc");
         CardAccount card = new CardAccount();
         card.deposit(1000);
         card.checkBalance();
-        card.withdraw(100.0);
+        card.withdraw(10000.0);
         card.checkBalance();
         System.out.println("Deposit acc");
         DepositAccount dep = new DepositAccount();
         dep.deposit(1000.0);
-        dep.withdraw(10.0);
+        dep.withdraw(10000.0);
         dep.checkBalance();
     }
 }
