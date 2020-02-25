26a792f3791d1b013dfa711a2b8585bd64689f46
Wtanga <pkkk.2011@mail.ru>
2020-02-25 21:31:55 +0700

    Flatmap

--- Module 7/Airport/src/Main.java
+++ Module 7/Airport/src/Main.java
@@ -6,25 +6,36 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
         Airport airport = Airport.getInstance();
         airport.getAllAircrafts();
-        LocalDateTime LocalMinus2Hours = LocalDateTime.now().minusHours(2);
-        LocalDateTime LocalPlus2Hours = LocalDateTime.now().plusHours(2);
+        LocalDateTime localMinus2Hours = LocalDateTime.now().minusHours(2);
+        LocalDateTime localPlus2Hours = LocalDateTime.now().plusHours(2);
         List<Terminal> listOfTerminals = airport.getTerminals();
         listOfTerminals.forEach(flight -> flight.getFlights()
                 .forEach(System.out::println));
         for (Terminal listOfTerminal : listOfTerminals) {
             for (Flight f : listOfTerminal.getFlights()) {
-                if (f.getType() == Flight.Type.ARRIVAL && toLocalDateTime(f.getDate()).isAfter(LocalMinus2Hours) && toLocalDateTime(f.getDate()).isBefore(LocalPlus2Hours) ) {
+                if (f.getType() == Flight.Type.ARRIVAL && toLocalDateTime(f.getDate()).isAfter(localMinus2Hours) && toLocalDateTime(f.getDate()).isBefore(localPlus2Hours) ) {
                     System.out.println("Прибывает");
                     System.out.println("Date: " + toLocalDateTime(f.getDate()) + "\nType: " +f.getAircraft());
 
                 }
             }
         }
+        // Stream FlatMap
+        Stream<Flight> arrivalList = listOfTerminals.stream()
+                .flatMap(terminal -> terminal.getFlights().stream());
+        arrivalList.forEach(flight -> {
+            if(flight.getType() == Flight.Type.ARRIVAL
+                    && toLocalDateTime(flight.getDate()).isAfter(localMinus2Hours)
+                    && toLocalDateTime(flight.getDate()).isBefore(localPlus2Hours))
+                System.out.println("Дата: " + toLocalDateTime(flight.getDate()) + "\nТип: " +flight.getAircraft());
+        });
+
     }
     public static LocalDateTime toLocalDateTime(Date dateToConvert) {
         return dateToConvert.toInstant()
