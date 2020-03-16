b3561b0ff531cc872994bb8562fae36fa3edd3ca
Wtanga <pkkk.2011@mail.ru>
2020-03-17 02:35:21 +0700

    files.walk

--- Module 9/File length reader/src/Main.java
+++ Module 9/File length reader/src/Main.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
@@ -7,20 +11,21 @@ public class Main {
     private static final int MB_MULTIPLIER = 1024 * 1024;
     private static final int GB_MULTIPLIER = 1024 * 1024 * 1024;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Input path");
         String path = scanner.nextLine();
-
-        File folder = new File(path);
-        if(folder.isDirectory()) {
-            System.out.println(folder.length() + " - bytes");
-            System.out.println(folder.length() / KB_MULTIPLIER + " - kilobytes");
-            System.out.println(folder.length() / MB_MULTIPLIER + " - megabytes");
-            System.out.println(folder.length() / GB_MULTIPLIER + " - gigabytes");
-        } else {
-            System.out.println("Invalid path");
-        }
+        Path folder = Paths.get(path);
+        System.out.println(Files.size(folder));
+        long size = Files.walk(folder)
+                .filter(p -> p.toFile().isFile())
+                .mapToLong(p -> p.toFile().length())
+                .sum();
+        System.out.println(size + " - bytes");
+        System.out.println((float) size / (float) KB_MULTIPLIER + " - kilobytes");
+        System.out.println((float) size / (float) MB_MULTIPLIER + " - megabytes");
+        System.out.println((float) size / (float) GB_MULTIPLIER + " - gigabytes");
     }
 }
+
