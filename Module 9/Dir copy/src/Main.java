62f3f6de7061d97caf208a3e979bb98348a0e577
Wtanga <pkkk.2011@mail.ru>
2020-04-20 18:36:55 +0700

    test

--- Module 9/Dir copy/src/Main.java
+++ Module 9/Dir copy/src/Main.java
@@ -1,23 +1,29 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Path originalPath = Paths.get("example/");
+    public static void main(String[] args) throws IOException {
+        Path originalPath = Paths.get("example");
         Path copiedPath = Paths.get("exampleCopied/");
         Files.walk(originalPath)
                 .forEach(file -> {
+                    Path currentPath = Paths.get(file.toAbsolutePath().toString());
                     if (Files.isDirectory(file)) {
-                        File folder = new File(copiedPath.getFileName().toString());
-                        folder.mkdir();
-                    } else {
-                        Path currentPath = Paths.get(file.toAbsolutePath().toString());
                         try {
-                            Files.copy(currentPath, copiedPath, StandardCopyOption.REPLACE_EXISTING);// копируем файл в папку относительно destination
+                            Files.createDirectory(copiedPath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+
+                        try {
+                            Path temp = Paths.get("exampleCopied/" + originalPath.toAbsolutePath().relativize(currentPath).toString());
+                            System.out.printf("%s -> %s%n", currentPath, temp);
+                            Files.copy(currentPath, temp, StandardCopyOption.REPLACE_EXISTING);// копируем файл в папку относительно destination
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
