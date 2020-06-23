6ac990b49886c3a3422408c2652cba644a6cce81
Egor <pkkk.2011@mail.ru>
2020-06-24 05:21:45 +0700

    done

--- Module 9/imgparser/src/Main.java
+++ Module 9/imgparser/src/Main.java
@@ -14,21 +14,22 @@ public class Main {
         String lenta = "https://lenta.ru/";
         try {
             Document doc = Jsoup.connect(lenta).maxBodySize(0).get();
-            doc.select("img").forEach(e -> {
-                try {
-                    URL url = new URL(e.attr("src"));
-                    // if (url.toString().contains(".jpg")) {
-
-                    InputStream in = new URL(url.toString()).openStream();
-                    Path temp = Paths.get("");
-                    Files.createDirectories(Paths.get(temp.toAbsolutePath() + url.getPath()));
-                    Files.copy(in, Paths.get(temp.toAbsolutePath() + url.getPath().replace(" //", "")), StandardCopyOption.REPLACE_EXISTING);
-                    in.close();
-                    //    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            });
+            doc.select("img")
+                    .stream()
+                    .filter(x -> x.attr("src")
+                            .startsWith("https"))
+                    .forEach(e -> {
+                        try {
+                            URL url = new URL(e.attr("src"));
+                            InputStream in = new URL(url.toString()).openStream();
+                            Path temp = Paths.get("");
+                            Files.createDirectories(Paths.get(temp.toAbsolutePath() + url.getPath()));
+                            Files.copy(in, Paths.get(temp.toAbsolutePath() + url.getPath().replace(" //", "")), StandardCopyOption.REPLACE_EXISTING);
+                            in.close();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    });
         } catch (IOException e) {
             System.out.println("Проблема при загрузке страницы");
         }
