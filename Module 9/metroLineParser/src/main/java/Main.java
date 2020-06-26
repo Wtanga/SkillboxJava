f01579b7e8e151cb809f474064562a534d200bff
Egor <pkkk.2011@mail.ru>
2020-06-27 00:15:53 +0700

    test

--- Module 9/metroLineParser/src/main/java/Main.java
+++ Module 9/metroLineParser/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -18,6 +17,14 @@ public class Main {
             doc.select("span.name")
                     .forEach(element -> {
                         System.out.println(element.text());
+                        try {
+                            Document doc1 = Jsoup.connect(metro).maxBodySize(0).get();
+                            doc1.select("span").forEach(element1 -> {
+                                System.out.println(element.text());
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     });
             doc.select("span[data-line]")
                     .forEach(element -> {
