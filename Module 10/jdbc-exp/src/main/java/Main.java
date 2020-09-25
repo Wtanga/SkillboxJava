6620f01c74e9be0a283cca73c990cc69a549d400
Egor <pkkk.2011@mail.ru>
2020-09-26 05:18:35 +0700

    add Hibernate

--- Module 10/jdbc-exp/src/main/java/Main.java
+++ Module 10/jdbc-exp/src/main/java/Main.java
@@ -1,3 +1,11 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
+
 import java.sql.*;
 
 public class Main {
@@ -22,5 +30,14 @@ public class Main {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        Course course = session.get(Course.class, 1);
+        System.out.println(course.getName());
+        sessionFactory.close();
     }
 }
