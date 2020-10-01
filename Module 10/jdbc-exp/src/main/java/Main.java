a5a29fc8cda44da761e44907943802f28011b011
Egor <pkkk.2011@mail.ru>
2020-10-02 05:07:40 +0700

    name + students count

--- Module 10/jdbc-exp/src/main/java/Main.java
+++ Module 10/jdbc-exp/src/main/java/Main.java
@@ -10,37 +10,38 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
-        String user = "root";
-        String password = "testtest";
+//        String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
+//        String user = "root";
+//        String password = "testtest";
+//
+//        try (Connection connection =
+//                     DriverManager.getConnection(url, user, password);
+//             Statement statement =
+//                     connection.createStatement();
+//             ResultSet resultSet =
+//                     statement.executeQuery("SELECT MAX(course_name) AS Name, " +
+//                             "COUNT(course_name) AS Count, " +
+//                             "TIMESTAMPDIFF(month, MIN(subscription_date), MAX(subscription_date)) AS Dif " +
+//                             "FROM PurchaseList pl WHERE YEAR(pl.subscription_date ) = 2018" +
+//                             " GROUP BY course_name ORDER BY pl.subscription_date;")) {
+//            while (resultSet.next()) {
+//                String courseName = resultSet.getString("Name");
+//                double count = resultSet.getInt("Count");
+//                double dif = resultSet.getInt("Dif");
+//                System.out.printf("%s %.3f \n",courseName, (count / dif));
+//            }
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-        try (Connection connection =
-                     DriverManager.getConnection(url, user, password);
-             Statement statement =
-                     connection.createStatement();
-             ResultSet resultSet =
-                     statement.executeQuery("SELECT MAX(course_name) AS Name, " +
-                             "COUNT(course_name) AS Count, " +
-                             "TIMESTAMPDIFF(month, MIN(subscription_date), MAX(subscription_date)) AS Dif " +
-                             "FROM PurchaseList pl WHERE YEAR(pl.subscription_date ) = 2018" +
-                             " GROUP BY course_name ORDER BY pl.subscription_date;")) {
-            while (resultSet.next()) {
-                String courseName = resultSet.getString("Name");
-                double count = resultSet.getInt("Count");
-                double dif = resultSet.getInt("Dif");
-                System.out.printf("%s %.3f \n",courseName, (count / dif));
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-//        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-//        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
-//        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-//
-//        Session session = sessionFactory.openSession();
-//
-//        Course course = session.get(Course.class, 1);
-//        System.out.println(course.getName());
-//        sessionFactory.close();
+        Session session = sessionFactory.openSession();
+
+        Course course = session.get(Course.class, 1);
+        System.out.println(course.getName() + "\nStudents count: " + course.getStudentsCount());
+
+        sessionFactory.close();
     }
 }
